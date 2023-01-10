7b0abc19adf5077c6a5ca537b59d298630986d8d
toad153 <76248516+toad153@users.noreply.github.com>
2023-01-10 21:47:41 +0530

    first commit

--- app.js
+++ app.js
@@ -75,6 +75,7 @@ app.post("/compose", function (req, res) {
 
 
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running on port 3000.");
 });
